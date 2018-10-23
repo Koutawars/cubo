989d786dc1efbd99673223a73b37d5682d2ce41a
Koutawars <admikouta@gmail.com>
2018-10-22 21:09:48 -0500

    Node

--- public/js/functions.js
+++ public/js/functions.js
@@ -48,3 +48,13 @@ function sumMatriz(a, b){
        return null;
    }
 }
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
